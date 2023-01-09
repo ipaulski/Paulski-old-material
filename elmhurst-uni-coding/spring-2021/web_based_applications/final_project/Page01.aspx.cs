2095507f5dc5532cb8541be62b5820dc712b0b12
ipaulski <60622792+ipaulski@users.noreply.github.com>
2023-01-09 13:33:14 -0600

    Add files via upload

---
+++ elmhurst-uni-coding/spring-2021/web_based_applications/final_project/Page01.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS440Practice
+{
+    public partial class Page01 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Thank you.";
+            Response.Redirect("Page02.aspx");
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtDOB.Text = "";
+            txtAddress.Text = "";
+            txtPhone.Text = "";
+        }
+    }
+}
---
+++ elmhurst-uni-coding/spring-2021/web_based_applications/final_project/Page03.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS440Practice
+{
+    public partial class Page03 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DisplayRecord();
+        }
+        private void DisplayRecord()
+        {
+            Record borrowerRecord = (Record)Session["Record"];
+            lblFirstName.Text = borrowerRecord.Borrower_FName;
+            lblLastName.Text = borrowerRecord.Borrower_LName;
+            lblDOB.Text = borrowerRecord.Borrower_DOB;
+            lblAddress.Text = borrowerRecord.Borrower_Address;
+            lblPhone.Text = borrowerRecord.Borrower_Phone;
+            lblBorrowerID.Text = Convert.ToString(borrowerRecord.Borrower_ID);
+            lblBooks.Text = borrowerRecord.Book_Title;
+            DateTime currentDate = DateTime.Today;
+            DateTime returnDate = DateTime.Today.AddDays(14);
+            lblIssued.Text = Convert.ToString(currentDate);
+            lblReturn.Text = Convert.ToString(returnDate);
+            borrowerRecord.Rec_IssueDate = currentDate;
+            borrowerRecord.Rec_ReturnDate = returnDate;
+            lblMessage.Text = borrowerRecord.Borrower_Msg;
+            Session["Record"] = borrowerRecord;
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Page02.aspx");
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            lblThankYou.Text = "Thank you for your request.\nWe will send a text message confirmation with your details.";
+        }
+    }
+}
