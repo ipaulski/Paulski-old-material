c1ff7a82d9c81a103c0f6080f89ee3e3a22f5830
ipaulski <60622792+ipaulski@users.noreply.github.com>
2023-01-09 12:02:26 -0600

    Add files via upload

---
+++ elmhurst-uni-coding/spring-2022/software_engineering/final_project/controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchGenerator.Models;
+using System.Diagnostics;
+
+
+namespace SearchGenerator.Controllers
+{
+    // Main controller
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //Action called by the "search" button in home page
+        public IActionResult btnsearch_Click()
+        {
+
+            // Create input object and populate it with field inputs
+            SearchInput userInput = new SearchInput();
+            userInput.certType = Request.Form["certType"];
+            userInput.certID = Request.Form["certID"];
+            userInput.lname = Request.Form["lname"];
+            userInput.fname = Request.Form["fname"];
+            userInput.city = Request.Form["city"];
+            userInput.state = Request.Form["state"];
+            userInput.country = Request.Form["country"];
+
+            // Place input into ViewBag object then redirect to results page
+            return RedirectToAction(actionName: "Index", controllerName: "Results", userInput);
+        }
+    }
+
+}
---
+++ elmhurst-uni-coding/spring-2022/software_engineering/final_project/controllers/ResultsController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchGenerator.Models;
+using System.Diagnostics;
+
+
+namespace SearchGenerator.Controllers
+{
+    public class ResultsController : Controller
+    {
+        private readonly ILogger<ResultsController> _logger;
+        private readonly SteelTankApplicationDbContext appContext;
+
+        public ResultsController(ILogger<ResultsController> logger, SteelTankApplicationDbContext ctxt)
+        {
+            appContext = ctxt;
+            _logger = logger;
+        }
+
+       
+        public IActionResult Index(SearchInput userInput)
+        {
+            // The certification type must have been chosen by user
+            // helps prevent user calling actoin through the url
+            if (userInput != null && userInput.certType != "")
+            {
+                obtainResults(userInput);
+                return View();
+            } else
+                return View("~/Views/Shared/Oops.cshtml");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // Runs query and populates results list
+        public IActionResult obtainResults(SearchInput userInput)
+        {
+
+            IQueryable<EmployeeData> certQuery;
+
+            if (userInput.certType == "SP001")
+            {
+                certQuery = (from employee in appContext.Sp001certifiedTankInspectorsLists                            
+                             select new EmployeeData
+                             {
+                                 id = employee.Id,
+                                 firstName = employee.First,
+                                 lastName = employee.Last,
+                                 expirationDate = employee.LicenseExpires,
+                                 phoneNumber = employee.Phone,
+                                 city = employee.City,
+                                 state = employee.St,
+                                 certification = "SP001"
+                             });
+            }
+            else if (userInput.certType == "CP")
+            {
+                certQuery = (from employee in appContext.CpcertifiedTestersLists
+                             select new EmployeeData
+                             {
+                                 id = employee.Id,
+                                 firstName = employee.First,
+                                 lastName = employee.Last,
+                                 expirationDate = employee.LicenseExpires,
+                                 phoneNumber = employee.Phone,
+                                 city = employee.City,
+                                 state = employee.St,
+                                 certification = "CP"
+
+                             });
+            }
+
+            // Handles improper action call
+            else
+                return View("~/Views/Shared/Oops.cshtml");
+
+            // all foreign countries are in the state columns of both docs 
+            //if US not chosen look for country in state colmn
+
+            // Filter out the query with the input fields from user
+            if (userInput.certID != null && userInput.certID != "")
+                certQuery = certQuery.Where(employee => employee.id.Contains(userInput.certID));
+
+            if (userInput.fname != null && userInput.fname != "")
+                certQuery = certQuery.Where(employee => employee.firstName.Contains(userInput.fname));
+
+            if (userInput.lname != null && userInput.lname != "")
+                certQuery = certQuery.Where(employee => employee.lastName.Contains(userInput.lname));
+            
+            if (userInput.city != null && userInput.city != "" )
+                certQuery = certQuery.Where(employee => employee.city.Contains(userInput.city));
+            
+            if (userInput.state != null && userInput.state != "" )
+                if(userInput.state != "ALL")
+                    certQuery = certQuery.Where(employee => employee.state.Contains(userInput.state));
+
+            if (userInput.country != null && userInput.country != "")
+                if (userInput.country != "ALL")
+                {
+                    if (userInput.country != "United States")
+                        certQuery = certQuery.Where(employee => employee.state.Contains(userInput.country));
+                    else if (userInput.state != null && userInput.state != "")
+                        if(userInput.state != "ALL")
+                            certQuery = certQuery.Where(employee => employee.state.Contains(userInput.state));
+                }
+
+            //Results returned from query
+            List<EmployeeData> results = certQuery.ToList();
+
+            // Sorts data by Last name
+            results.Sort((empOne, empTwo) => String.Compare(empOne.lastName, empTwo.lastName));
+            return View(results);
+        }
+    }
+
+
+}
+
